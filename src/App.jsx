28ff04d67a90946695a6bcab047e34eb959bea35
Claudia <yayita0577@gmail.com>
2022-10-10 18:26:27 -0500

    Se creo header y lista paciente
    Estoy  importando y agregue un h1 y h2

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//nucleo de mi proyecto
+//une los componentes
+
+//import ',/App.css'
+import Header from"./componentes/Header"
+import ListaPaciente from "./componentes/listaPaciente"
+function App() {
+
+  return(
+    <>
+    <Header/>
+    <ListaPaciente/>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/listaPaciente.jsx
@@ -0,0 +1,7 @@
+function ListaPaciente (){
+    return(
+        <h2>Lista Paciente: </h2>
+    )
+}
+
+export default ListaPaciente
