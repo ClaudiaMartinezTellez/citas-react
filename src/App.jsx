68b22eef658149035103b49c253891e811cc794f
ClaudiaMartinezTellez <112780006+ClaudiaMartinezTellez@users.noreply.github.com>
2022-10-12 15:49:46 -0500

    Agragando componentes
    Se agregaron textos con h2 a los componentes en linea 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@
 //une los componentes
 
 //import ',/App.css'
+import Formulario from "./componentes/formulario"
 import Header from"./componentes/Header"
 import ListaPaciente from "./componentes/listaPaciente"
+import Pacientes from "./componentes/pacientes"
+import Error from "./componentes/error"
 function App() {
 
   return(
     <>
     <Header/>
     <ListaPaciente/>
+    <Formulario/>
+    <Error/>
+    <Pacientes/>
 
 
     </>
